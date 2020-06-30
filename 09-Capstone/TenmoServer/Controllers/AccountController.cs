5ba1200fddec902566ddaddd7d4fe47fddc4c8b9
Matt Theis <mdtheis@gmail.com>
2020-06-30 16:48:42 -0400

    uncommented controller

--- 09-Capstone/TenmoServer/Controllers/AccountController.cs
+++ 09-Capstone/TenmoServer/Controllers/AccountController.cs
@@ -1,23 +1,24 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Threading.Tasks;
-//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace TenmoServer.Controllers
-//{
-//    [Route("[controller]")]
-//    [ApiController]
-//    public class AccountController : ControllerBase
-//    {
-//        [HttpGet]
-//        [Authorize]
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
 
-//        public ActionResult<List<AccountController>> GetAccounts()
-//        {
-//            string name = User.Identity.Name;
-//        }
-//    }
-//}
+        public ActionResult<List<AccountController>> GetAccounts()
+        {
+            string name = User.Identity.Name;
+            return null;
+        }
+    }
+}
