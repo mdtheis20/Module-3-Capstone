a53581b1bc0b8c45fe6e4f16fd84c01738bc7c51
Matt Theis <mdtheis@gmail.com>
2020-07-02 13:19:20 -0400

    started transfer sql dao

---
+++ 09-Capstone/TenmoServer/DAO/TransferSqlDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TenmoServer.Models;
+using TenmoServer.Security.Models;
+using TenmoServer.Security;
+
+namespace TenmoServer.DAO
+{
+    public class TransferSqlDAO : ITransferDAO
+    {
+
+        private readonly string connectionString;
+
+        public TransferSqlDAO(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+
+        public Transfer CreateTransfer
+    }
+}
